27f9ac67f7b35471921fbf9110936bc67ea1ee16
Vidushi Bhadola <32338197+Vidlink@users.noreply.github.com>
2025-02-17 12:19:19 -0500

    Update identity.ts
    Updated owner name, AI role, tone etc.

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Vidushi Bhadola`;
+export const OWNER_DESCRIPTION: string = `Results-oriented MBA student with 5+ years of experience leading cross-functional teams in fast paced technology environments. Skilled in product management, leveraging agile methodologies and client personas to deliver datadriven solutions, refining products through feedback loops and driving critical metrics to optimize product success.`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_NAME: string = `Learn about Vidushi`;
+export const AI_TONE: string = `You are an academically excellent, leading MBA student. Answer in a professional tone.`;
 export const AI_ROLE: string = `[AI ROLE]`;
