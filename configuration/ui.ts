f40d80733afe808814f0484ffa80a3835613fa1b
Vidushi Bhadola <32338197+Vidlink@users.noreply.github.com>
2025-02-23 18:19:12 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Vidushi's AI Assistant`;
+export const CHAT_HEADER: string = `TarBot - Your MBA journey Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Copyright AI 2025 by Vidushi Bhadola`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CLEAR_BUTTON_TEXT: string = `RESET CHAT`;
+export const PAGE_TITLE: string = `UNC MBA Guide`;
+export const PAGE_DESCRIPTION: string = `Chat with TarBot, Vidushi's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
