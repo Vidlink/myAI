c703fcb76812732a624b7d3c45688ff40f6f6f28
Vidushi Bhadola <32338197+Vidlink@users.noreply.github.com>
2025-02-28 18:02:16 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `TarBot - Your MBA journey Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Copyright AI 2025 by Vidushi Bhadola`;
 export const CLEAR_BUTTON_TEXT: string = `RESET CHAT`;
-export const PAGE_TITLE: string = `UNC MBA Guide`;
+export const PAGE_TITLE: string = `TarBot - Your MBA journey Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with TarBot, Vidushi's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
